e98dfdf06da51d7e064136ec4c373e0daf764d24
Sri <kssrivatsaan@gmail.com>
2021-01-09 16:21:20 -0500

    before updating

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/WobbleGoalCollector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/WobbleGoalCollector.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.GameChangersTester;
 
+import ftc.electronvolts.util.PIDController;
 import ftc.evlib.hardware.motors.Motor;
 import ftc.evlib.hardware.sensors.AnalogSensor;
 import ftc.evlib.hardware.servos.ServoControl;
@@ -11,6 +12,13 @@ public class WobbleGoalCollector {
     private Enum close;
     private Enum open;
     private AnalogSensor potentiometer;
+    private PIDController pidController;
+
+
+    //positions
+    private final double upPosition = 0;
+    private final double downPosition = 0;
+
 
     public WobbleGoalCollector(Motor rotation, ServoControl pinch, Enum close, Enum open, AnalogSensor potentiometer) {
         this.rotation = rotation;
@@ -18,6 +26,8 @@ public class WobbleGoalCollector {
         this.close = close;
         this.open = open;
         this.potentiometer = potentiometer;
+        // NEED TO TUNE IT!!!!!!!!!!!!!!!!!!!!!!!!!
+        pidController = new PIDController(0.4,0.5,2.5,5.4);
     }
 
     public void close() {
@@ -25,6 +35,14 @@ public class WobbleGoalCollector {
     }
     public void open() {
         pinch.goToPreset(open);
+    }
+
+    public void moveArmUp() {
+
+    }
+
+    public void moveArmDown(){
+
     }
 
 
