0cfe5497a50a889b8e964dc1a823117b8df4dd6b
FalseMSP <55561022+FalseMSP@users.noreply.github.com>
2021-02-07 20:04:06 -0500

    Added BlinkyControl.
    It makes it so you can change the color of LED lights.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/BlinkyControl.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.GameChangersTester;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+
+public class BlinkyControl {
+    private final RevBlinkinLedDriver blinky;
+    private RevBlinkinLedDriver.BlinkinPattern currentPattern;
+    private RevBlinkinLedDriver.BlinkinPattern newPattern;
+
+    public BlinkyControl(RevBlinkinLedDriver blinky, RevBlinkinLedDriver.BlinkinPattern startPattern) {
+        this.blinky = blinky;
+        currentPattern = startPattern;
+        newPattern = startPattern;
+    }
+
+    public void setNewPattern(RevBlinkinLedDriver.BlinkinPattern pattern) {
+        newPattern = pattern;
+    }
+
+    public void update() {
+        if (newPattern != currentPattern){
+            blinky.setPattern(newPattern);
+            currentPattern = newPattern;
+        }
+    }
+}
