587fc983c390c2309fcf14fe0016ae2ee8043fa0
Abhi Divvela <saidivvela2005@gmail.com>
2021-02-15 17:46:36 -0500

    added state for blinkin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/BlinkEventListener.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/BlinkEventListener.java
@@ -1,17 +1,24 @@
 package org.firstinspires.ftc.teamcode.GameChangersTester;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlinkEventListener {
 
-    private List<BlinkEvent> internalList;
+    private final List<BlinkEvent> internalList = new ArrayList<>();
 
     public synchronized void requestNewBlinkPattern(BlinkEvent event) {
         internalList.add(event);
     }
 
-    public synchronized List<BlinkEvent> extractEvents() {
-        return
+    public synchronized BlinkEvent extractEvents() {
+        if(internalList.isEmpty()) {
+            return null;
+        } else {
+            BlinkEvent event = internalList.get(internalList.size() - 1);
+            internalList.clear();
+            return event;
+        }
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameChangersTester/BlinkState.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.GameChangersTester;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+
+import java.util.List;
+
+import ftc.electronvolts.statemachine.BasicAbstractState;
+import ftc.electronvolts.statemachine.State;
+import ftc.electronvolts.statemachine.StateName;
+
+public class BlinkState extends BasicAbstractState {
+
+    private final BlinkEventListener listener;
+    private final List<RevBlinkinLedDriver.BlinkinPattern> blinkinPatternList;
+    private final List<Long> timeList;
+    private int currentPatternIndex;
+    private final RevBlinkinLedDriver blinkin;
+    long [] endTimes;
+    private StateName nextState;
+
+    public BlinkState(BlinkEventListener listener, List<RevBlinkinLedDriver.BlinkinPattern> blinkinPatternList, List<Long> timeList, RevBlinkinLedDriver blinkin) {
+        this.listener = listener;
+        this.blinkinPatternList = blinkinPatternList;
+        this.timeList = timeList;
+        this.blinkin = blinkin;
+        endTimes = new long[timeList.size()];
+    }
+
+    @Override
+    public void init() {
+        long cycleTime = System.currentTimeMillis();
+        blinkin.setPattern(blinkinPatternList.get(0));
+        currentPatternIndex = 0;
+        long delta = 0;
+        for (int i = 0; i < timeList.size(); i++) {
+            delta = delta + timeList.get(i);
+            endTimes[i] = cycleTime + delta;
+        }
+    }
+
+    @Override
+    public boolean isDone() {
+        BlinkEvent currentEvent = listener.extractEvents();
+        if(currentEvent != null) {
+            nextState = currentEvent;
+            return true;
+        }
+        long currentTime = System.currentTimeMillis();
+        int newPatternIndex = currentPatternIndex;
+        for (int i = endTimes.length - 1; i >= 0; i--) {
+            if(currentTime > endTimes[i]) {
+                newPatternIndex = i + 1;
+                break;
+            }
+        }
+        if(newPatternIndex != currentPatternIndex) {
+            if(newPatternIndex > blinkinPatternList.size()) {
+                init();
+            } else {
+                currentPatternIndex = newPatternIndex;
+                blinkin.setPattern(blinkinPatternList.get(currentPatternIndex));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public StateName getNextStateName() {
+        return nextState;
+    }
+}
